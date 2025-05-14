41493f698acb939e0d058fd52deb292d6a1e78ce
Roy Singh <roysingh@Roys-MacBook-Air.local>
2025-05-14 12:39:12 +0530

    changes

---
+++ _creates_modules/src/english/greetings.rs
@@ -0,0 +1,9 @@
+pub mod greeting{
+    pub fn modules_hello() -> String {
+        return "StringConverted".to_string();
+    }
+     pub struct Details {
+        pub name: String,
+        pub email: String
+     }
+}
---
+++ _creates_modules/src/main.rs
@@ -0,0 +1,20 @@
+mod english;
+mod parent {
+    pub fn get_by_id() { 
+        println!("This is the function inside the modules");
+    }
+
+    pub mod child1 {
+        // Additional functionality can go here
+    }
+}
+
+use parent::get_by_id as getfunction;
+
+use english::greetings;
+
+fn main() {
+    getfunction(); // Just call the function, no need to println! its result
+    let data_comes : String = greetings::greeting::modules_hello();
+    print!("{}", data_comes);
+}
