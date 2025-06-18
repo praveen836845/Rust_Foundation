0fd032b9197eda11305c8187162187a40fc17973
Roy Singh <roysingh@Roys-MacBook-Air.local>
2025-06-18 17:21:55 +0530

    changes

--- _traits_Import_concept/trait_as_param/src/main.rs
+++ _traits_Import_concept/trait_as_param/src/main.rs
@@ -1,13 +1,14 @@
 #[derive(Debug)] // Added Debug derive for the struct
-struct DataStorage<T> { // Rust convention uses PascalCase for type names
+struct DataStorage<T> {
+    // Rust convention uses PascalCase for type names
     values: i32,
-    generics_data: T, // snake_case for field names
+    genericsdata: T, // snake_case for field names
 }
 
 trait MethodSignature {
     // Default method that will call another function with self as parameter
     fn default_method(&self) {
-        println!("Calling default_method from MethodSignature");
+        println!("Called the Default Method and Now calling the Sibling functions in the trait");
         self.called_function(); // Call the implemented method
     }
 
@@ -15,7 +16,7 @@ trait MethodSignature {
     fn called_function(&self);
 }
 
-// Implementing the trait for DataStorage<T>  
+// Implementing the trait for DataStorage<T>
 // When implementing a trait for a generic struct, the impl must account for generics:
 
 impl<T: std::fmt::Debug> MethodSignature for DataStorage<T> {
@@ -27,13 +28,25 @@ impl<T: std::fmt::Debug> MethodSignature for DataStorage<T> {
     }
 }
 
+fn trait_as_parameters(value: impl MethodSignature) -> impl MethodSignature {
+    println!(
+        "I'm the Function with papermerter as Trait  on shield type : values"
+    );
+    value.called_function();
+    return DataStorage {
+        values: 32,
+        genericsdata: "Stringvalue".to_string(),
+    };
+}
+
 fn main() {
-    println!("When the Implementing the generics Struct then impl block also must be generics");
-    
+    println!("Creating instance and Calling the Default Method that are implmented in the Trait");
+
     let instance = DataStorage {
         values: 23,
-        generics_data: "Hello_generics_key".to_string(),
+        genericsdata: "Hello_generics_key".to_string(),
     };
-    
+
     instance.default_method();
+    trait_as_parameters(instance);
 }
